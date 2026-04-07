e636b0a3657bdf7adc8cbe663149933e60f426fb
MenaceHecker <thedynamic2508@gmail.com>
2026-04-06 20:54:52 -0400

    Changed the job service algorithm

--- services/job_service.py
+++ services/job_service.py
@@ -1,16 +1,26 @@
-from services.db import cursor, conn
+from services.db import conn, cursor
+
 
 def save_jobs(jobs):
+    inserted = 0
+
     for job in jobs:
-        cursor.execute("""
-        INSERT INTO jobs (title, company, location, link, description, source)
-        VALUES (?, ?, ?, ?, ?, ?)
-        """, (
-            job["title"],
-            job["company"],
-            job["location"],
-            job["link"],
-            job["description"],
-            job["source"]
-        ))
+        try:
+            cursor.execute("""
+            INSERT INTO jobs (title, company, location, link, description, source)
+            VALUES (?, ?, ?, ?, ?, ?)
+            """, (
+                job.title,
+                job.company,
+                job.location,
+                job.link,
+                job.description,
+                job.source,
+            ))
+            inserted += 1
+        except Exception:
+            # likely duplicate due to UNIQUE link
+            continue
+
     conn.commit()
+    return inserted
